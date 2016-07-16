d3c26da7f22a523c1074fea51ac3ab101e259c4c
Kevin Zhu <kevinazhu@gmail.com>
2016-07-16 10:48:29 -0700

    fixed hidden boxes if suggestions less than 6

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,17 +1,12 @@
 $.get('/amazonData', function(data, status) {
-	console.log(data);
 	var newdata = JSON.parse(data);
-	console.log(newdata);
-	document.getElementById("a0").href = newdata[0].url;
-	document.getElementById("i0").src = newdata[0].image;
-	document.getElementById("a1").href = newdata[1].url;
-	document.getElementById("i1").src = newdata[1].image;
-	document.getElementById("a2").href = newdata[2].url;
-	document.getElementById("i2").src = newdata[2].image;
-	document.getElementById("a3").href = newdata[3].url;
-	document.getElementById("i3").src = newdata[3].image;
-	document.getElementById("a4").href = newdata[4].url;
-	document.getElementById("i4").src = newdata[4].image;
-	document.getElementById("a5").href = newdata[5].url;
-	document.getElementById("i5").src = newdata[5].image;
+	for(var i = 0; i < newdata.length; i++) {
+		document.getElementById("a" + i).href = newdata[i].url;
+		document.getElementById("i" + i).src = newdata[i].image;
+	}
+
+	while(i < 6) {
+		document.getElementById("a" + i).style.visibility = "hidden";
+		i++;
+	}
 })
