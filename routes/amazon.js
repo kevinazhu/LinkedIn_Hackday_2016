a580577b6d23a57385237066535603db8e69e872
Kevin Zhu <kevinazhu@gmail.com>
2016-07-15 23:04:27 -0700

    added amazon retrieval. need to fix react code

--- routes/amazon.js
+++ routes/amazon.js
@@ -5,20 +5,47 @@ var amazon = require('amazon-product-api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var client = amazon.createClient({
+	var array = ["bento", "diy", "love", "chocolate"];
+	var data = combinations(array);
+
+	amazonClient = amazon.createClient({
 		awsId: "AKIAJ2QRGNUKTH4WJRLQ",
 		awsSecret: "***REMOVED***"
 	});
-	client.itemSearch({
-		keywords: 'headphones'
-	}, function(err, results, response) {
-		if (err) {
-			res.send(err)
-		} else {
-			console.log(response); // response (containing TotalPages, TotalResults, MoreSearchResultsUrl and so on)
-			res.send(results);
-		}
-	});
+
+	var i = 0;
+	var items = []
+	searchItem(data, i, items, res);
 });
+
+var combinations = function(array) {
+	combination_data = []
+	for(var i = 0; i < array.length; i++) {
+		for(var j = i + 1; j < array.length; j++) {
+			combination_data.push(array[i] + " " + array[j]);
+		}
+	}
+	return combination_data;
+}
+
+var searchItem = function(data, i, items, res) {
+	if(i < data.length) {
+		amazonClient.itemSearch({
+			keywords: data[i]
+		}, function(err, results, response) {
+			if (err) {
+				console.log(err)
+			} else {
+				for(var num = 0; num < results.length; num++) {
+					items.push({ url: results[num]["DetailPageURL"][0], name: results[num]["ItemAttributes"][0]["Title"][0] });
+				}
+				
+				searchItem(data, i+1, items, res);
+			}
+		});
+	} else {
+		res.send(items);
+	}
+}
 
 module.exports = router;
