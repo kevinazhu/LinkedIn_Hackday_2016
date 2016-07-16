824b6e41a65564839d5147edff182bc971c293a1
Dong Joon Kim <dokim@dokim-mn1.linkedin.biz>
2016-07-16 02:37:03 -0700

    Added action events

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,21 @@ router.get('/', function(req, res, next) {
 router.post('/loading', function(req, res, next) {
   res.render('loading');
   console.log(req.body.link);
+
+  // parce the link and check if the link is valid
+
+  // if Pinterest or Facebook,
+  // parse words from Pinterest or Facebook
+  // launch Amazon action
+  // when it's done, Amazon action should launch /landing page and set
+  // HTML elements with photo, link to the product, and name of the product
+
+  // if LinkedIn, launch hard-coded Amazon action with Programming books
+  // launch hard-coded Amazon action
+  // when it's done, Amazon action should launch /landing page and set
+  // HTML elements with photo, link to the product, and name of the product
+
+  // otherwise, throw invalid link action
 });
 
 module.exports = router;
