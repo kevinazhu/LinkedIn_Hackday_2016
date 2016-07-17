b8a2dec0588c5fc08d4ef38293a3c94bd51e0f6b
Kevin Zhu <kevinazhu@gmail.com>
2016-07-17 16:35:28 -0700

    cleaned up modals

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,38 +1,6 @@
 $.get('/amazon?data0=' + words["data0"] + '&data1=' + words["data1"] + '&data2=' + words["data2"] + '&data3=' + words["data3"],function(data, status) {
 	amazonData = JSON.parse(data);
 
-	/*
-	for(var i = 0; i < amazonData.length; i++) {
-		document.getElementById("a" + i).href = amazonData[i]["url"];
-		document.getElementById("i" + i).src = amazonData[i]["image"];
-	}
-
-	while(i < 6) {
-		document.getElementById("a" + i).style.visibility = "hidden";
-		i++;
-	}
-
-	var suggestions = [{
-	  url: 'http://google.com',
-	  img: 'img/portfolio/cabin.png'
-	}, {
-	  url: 'http://yahoo.com',
-	  img: 'img/portfolio/cabin.png'
-	}, {
-	  url: 'http://linkedin.com',
-	  img: 'img/portfolio/cabin.png'
-	}, {
-	  url: 'http://facebook.com',
-	  img: 'img/portfolio/cabin.png'
-	}, {
-	  url: 'http://bing.com',
-	  img: 'img/portfolio/cabin.png'
-	}, {
-	  url: 'http://yelp.com',
-	  img: 'img/portfolio/cabin.png'
-	}];
-	*/
-
 	var List = function List(props) {
 	  return React.createElement(
 	    'div',
