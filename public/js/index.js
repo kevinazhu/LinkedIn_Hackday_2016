7789e2081d30fcb75437138229532db967c7a3e1
Kevin Zhu <kevinazhu@gmail.com>
2016-07-18 03:07:10 -0700

    added instructions and removed mobile menu button

--- public/js/index.js
+++ public/js/index.js
@@ -5,10 +5,8 @@ $(document).ready(function() {
 
 	$("#gift-pic").click(function() {
 	    $('html, body').animate({
-	        scrollTop: $("#find-gift").offset().top
-	    }, 1000, "swing", function() {
-	    	window.location.href = "/#find-gift";
-	    });
+	        scrollTop: $("#about").offset().top
+	    }, 1000);
 	});
 });
 
